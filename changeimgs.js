7cf8938ff9a5544a8ebbf096c37a34fce2481cbf
JET-6 <thomas.jimi@gmail.com>
2021-05-23 10:39:42 -0700

    testing updates
    header didn't work last time?

---
+++ changeimgs.js
@@ -0,0 +1,15 @@
+// a bit of work on changing images
+function changeImg() {
+  image = document.querySelector('#img');
+  img.src = url("images/night.jpg")
+}
+
+function weGo() {
+  setTimeout(partOne, 1000);
+}
+
+
+function functionS() {
+  weGo();
+  changeImg();
+}
