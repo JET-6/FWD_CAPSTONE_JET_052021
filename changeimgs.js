1a1b30ebcfb837434eaee75416803b1e2e419501
JET-6 <thomas.jimi@gmail.com>
2021-05-25 16:13:41 -0700

    v1.21
    more js to clean up as story progresses
    
    some css fiddling

--- changeimgs.js
+++ changeimgs.js
@@ -2,15 +2,23 @@
 function changeImg() {
   let image = document.querySelector('#img');
   roughNight = `url("images/night.jpg")`;
-  image.style.backgroundImage = roughNight;
+  document.body.style.backgroundImage = roughNight;
+  document.body.style.backgroundSize = 'cover';
+  document.body.style.backgroundPosition = 'top';
 }
 
 function weGo() {
   setTimeout(partOne, 1000);
 }
 
+function hideaway() {
+  document.querySelector('.title').style.display='none';
+  document.querySelector('#img').style.display='none';
+  document.querySelector('.container').style.opacity ='15%';
+}
 
 function functionS() {
   weGo();
   changeImg();
+  hideaway();
 }
